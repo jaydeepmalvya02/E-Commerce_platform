8be41a0483bbf8a28b56fb7f3fab5087c4faa43f
jaydeepmalvya02 <jaydeepmalvya02@gmail.com>
2025-04-03 02:14:19 +0530

    Completed Home->Hero and gender collection and till newArrival  section 2:15:32

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,12 +1,15 @@
 import {BrowserRouter, Routes,Route} from 'react-router-dom';
 import UserLayout from './components/Layout/UserLayout';
+import Home from './pages/Home';
 function App() {
  
 
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<UserLayout/>}>{/* UserLayout */}</Route>
+        <Route path='/' element={<UserLayout/>}>{/* UserLayout */}
+        <Route index element={<Home/>}></Route>
+        </Route>
         <Route> {/* AdminLayout */}</Route>
       </Routes>
     </BrowserRouter>
