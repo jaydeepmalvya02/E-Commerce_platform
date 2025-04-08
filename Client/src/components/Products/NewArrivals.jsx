e649511d57156154b94d8340fcdef98e083a57ee
Jaydeepmalvya02 <jaydeepmalvya02@gmail.com>
2025-04-07 23:56:00 -0700

    Home page is Completed till 3:18:00

--- Client/src/components/Products/NewArrivals.jsx
+++ Client/src/components/Products/NewArrivals.jsx
@@ -136,13 +136,13 @@ const NewArrivals = () => {
 
     }
 
-    console.log({
-      scrollLeft:container.scrollLeft,
-      clientWidth:container.clientWidth,
-      containerScrollWidth:container.scrollWidth,
-      offsetLeft: scrollRef.current.offsetLeft,
+    // console.log({
+    //   scrollLeft:container.scrollLeft,
+    //   clientWidth:container.clientWidth,
+    //   containerScrollWidth:container.scrollWidth,
+    //   offsetLeft: scrollRef.current.offsetLeft,
 
-    });
+    // });
     
   }
   useEffect(() => {
