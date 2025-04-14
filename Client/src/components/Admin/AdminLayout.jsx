d63ce256045f45813a073025cce9ef86a46a2bce
Jaydeepmalvya02 <jaydeepmalvya02@gmail.com>
2025-04-14 05:08:00 -0700

    correct the ui occur 5:55;35

--- Client/src/components/Admin/AdminLayout.jsx
+++ Client/src/components/Admin/AdminLayout.jsx
@@ -11,11 +11,11 @@ const AdminLayout = () => {
   return (
     <div className="min-h-screen flex flex-col md:flex-row relative">
       {/* Mobile Toggle Button */}
-      <div className="flex md:screen p-4 bg-gray-900 text-white z-20">
+      <div className="flex md:hidden p-4 bg-gray-900 text-white z-20">
         <button onClick={toggleSidebar}>
           <FaBars size={24} />
         </button>
-        <h1 className="ml-4 text-xl font-medium">Admin Dashboard</h1>
+        {/* <h1 className="ml-4 text-xl font-medium">Admin Dashboard</h1> */}
       </div>
       {/* Overlay for mobile sidebar */}
       {isSidebarOpen && (
