d2a5d8e0c4a1800c1df9c59813ed64e9769ddd50
Jaydeepmalvya02 <jaydeepmalvya02@gmail.com>
2025-04-15 05:49:15 -0700

    Create Product Api complete till 8:23:31

---
+++ Api/routes/ProductRoutes.js
@@ -0,0 +1,66 @@
+const express = require("express");
+const Product = require("../models/ProductModel");
+const { protect } = require("../middleware/authMiddleware");
+const router = express.Router();
+
+
+// @Route POST /api/products
+// desc Create a new Product
+// access Private/admin
+
+router.post('/',protect,async(req , res )=>{
+    try {
+        const {
+            name,
+            description,
+            price,
+            discountPrice,
+            countInStock,
+            category,
+            brand,
+            sizes,
+            colors,
+            collections,
+            material,
+            gender,
+            images,
+            isFeatured,
+            isPublished,
+            tags,
+            dimensions,
+            weight,
+            sku,
+
+        }=req.body
+        const product =new Product({
+            name,
+            description,
+            price,
+            discountPrice,
+            countInStock,
+            category,
+            brand,
+            sizes,
+            colors,
+            collections,
+            material,
+            gender,
+            images,
+            isFeatured,
+            isPublished,
+            tags,
+            dimensions,
+            weight,
+            sku,
+            user:req.user._id,  //Reference to the admin user who created it 
+
+        })
+
+        const createdProduct=await product.save()
+        res.status(201).json(createdProduct)
+    } catch (error) {
+        console.error(error)
+        res.status(500).send("Server Error")
+    }
+})
+module.exports=router
--- Api/server.js
+++ Api/server.js
@@ -3,6 +3,7 @@ const cors=require('cors')
 const dotenv=require('dotenv')
 const connectDB=require('./config/db.js')
 const userRoutes=require('./routes/userRoutes.js')
+const productRoutes=require('./routes/ProductRoutes.js')
 
 dotenv.config()
 
@@ -19,6 +20,7 @@ app.get("/",(req,res)=>{
 })
 // API Routes
 app.use('/api/users',userRoutes)
+app.use('/api/products',productRoutes)
 
 
 app.listen(PORT,()=>{
