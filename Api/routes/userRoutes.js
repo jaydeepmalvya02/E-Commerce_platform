eb31b07c13184430505d54186d6787815233c6d3
Jaydeepmalvya02 <jaydeepmalvya02@gmail.com>
2025-04-15 04:12:57 -0700

    Complete User Apis till 8:05:30

---
+++ Api/routes/userRoutes.js
@@ -0,0 +1,125 @@
+const express = require("express");
+const User = require("../models/UserModel.js");
+const jwt = require("jsonwebtoken");
+const {protect}=require('../middleware/authMiddleware.js')
+const router = express.Router();
+
+// @route POST /api/users/register
+// @desc Register a new user
+// access public
+
+router.post("/register", async (req, res) => {
+  const { name, email, password } = req.body;
+  try {
+    // Registration logic
+    let user = await User.findOne({ email });
+    if (user) return res.status(400).json({ message: "User already exists" });
+    user = new User({ name, email, password });
+
+    await user.save();
+
+    //    Create JWT Payload
+    const payload = {
+      user: {
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+        password: user.password,
+        role: user.role,
+      },
+    };
+    // Sign and return the token along with user data
+    jwt.sign(
+      payload,
+      process.env.JWT_SECRET,
+      { expiresIn: "40h" },
+      (err, token) => {
+        if (err) throw err;
+        // Send the user and token in response
+        res.status(201).json({
+          user: {
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            password: user.password,
+            role: user.role,
+          },
+          token,
+        });
+      }
+    );
+    // res.status(201).json({
+    //     user:{
+    //         _id:user._id,
+    //         name:user.name,
+    //         email:user.email,
+    //         password:user.password,
+    //         role:user.role
+    //     }
+    // })
+  } catch (error) {
+    console.log(error);
+    res.status(500).send("Server Error");
+  }
+});
+
+// @Route POST/api/users/login
+// @desc Authenticate user
+// @Access public
+router.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    // find the user by email
+    const user = await User.findOne({ email });
+    if (!user) res.status(400).json({ message: "Invalid Credentials" });
+    const isMatch = await user.matchPassword(password);
+    if (!isMatch) return (400).json({ message: "Invalid Credentials" });
+
+    //    Create JWT Payload
+    const payload = {
+      user: {
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+        password: user.password,
+        role: user.role,
+      },
+    };
+    // Sign and return the token along with user data
+    jwt.sign(
+      payload,
+      process.env.JWT_SECRET,
+      { expiresIn: "40h" },
+      (err, token) => {
+        if (err) throw err;
+        // Send the user and token in response
+        res.json({
+          user: {
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            password: user.password,
+            role: user.role,
+          },
+          token,
+        });
+      }
+    );
+  } catch (error) {
+    console.log(error);
+
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+
+// @route GET /api/user/profile
+// @desc Get logged-in user's profile (protected route)
+// @access private
+router.get('/profile',protect ,async(req,res)=>{
+    console.log(req.user);
+    
+    res.json(req.user)
+})
+module.exports = router;
---
+++ Api/server.js
@@ -0,0 +1,27 @@
+const express=require('express')
+const cors=require('cors')
+const dotenv=require('dotenv')
+const connectDB=require('./config/db.js')
+const userRoutes=require('./routes/userRoutes.js')
+
+dotenv.config()
+
+const app=express()
+app.use(express.json())
+app.use(cors());
+
+const PORT=process.env.PORT||3000
+// Connect to MongoDB
+connectDB()
+
+app.get("/",(req,res)=>{
+    res.send("Welcome to Rabbit api")
+})
+// API Routes
+app.use('/api/users',userRoutes)
+
+
+app.listen(PORT,()=>{
+    console.log(`Server is running on http://localhost:${PORT}`);
+    
+})
